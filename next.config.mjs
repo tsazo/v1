16793389e72e8eac58758ce357e607e6a4d3d1bb
Trinity Sazo <trinitysazo@gmail.com>
2024-04-06 15:32:00 -0400

    Adding skeleton for image permissions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: {
+            protocol: "https",
+            hostname: "", // Main host url
+        }
+    }
+};
 
 export default nextConfig;
